8ac93e8e3e55e7017027c341f9e3e861b3f4546d
SANKET SINGH <singhgurger@gmail.com>
2021-01-23 09:59:57 +0000

    code review changes

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const readLineSync = require('readline-sync');
 
 function welcome()
 {
-const welcome = readLineSync.question('\nAre you ready for the quiz? ' + '\n');
+const welcome = readLineSync.question('\n Hello , Are you ready for the quiz? Please Type (yes/No)' + '\n');
 
-if(welcome == "yes"){
+if(welcome === "yes"){
 console.log("Good,let's Start");
 main();
 }
@@ -14,55 +14,34 @@ return;
 }
 }
 
-var myquestions =[
+var questions =[
     {
-      question : " The C++ language is" ,
-      answers  : {
-          a : 'Pure Object oriented',
-          b : 'Not Object oriented',
-          c : 'Semi Object-oriented or Partial Object-oriented',
-          d : 'None of the above'
-      },
-      correctAnswer : 'c'
+      question       : " The C++ language is" ,
+      options        : ['Pure Object oriented','Not Object oriented','Semi Object-oriented or Partial Object-oriented','None of the above'],
+      correctAnswer  : 'c'
     },
     {
-      question : "Which of the following class allows to declare only one object of it?",
-      answers  :{
-          a : 'Abstract class',
-          b : 'Virtual class',
-          c : 'Singleton class',
-          d : 'Friend class'
-      },
-      correctAnswer : 'c'
+      question       : "Which of the following class allows to declare only one object of it?",
+      options        :['Abstract class','Virtual class','Singleton class','Friend class'],
+    
+      correctAnswer  : 'c'
     },
     {
-      question : " Which of the following is the correct syntax for declaring the array?",
-      answers  :{
-          a : 'init array []',
-          b : 'int array [5];',
-          c : 'Array[5];',
-          d : 'None of the above'
-      },
+      question      : " Which of the following is the correct syntax for declaring the array?",
+      options       : ['init array []','int array [5];','Array[5];','None of the above'],
+         
       correctAnswer : 'b'
     },
     {
-      question : " Which type of memory is used by an Array in C++ programming language?",
-      answers  :{
-          a : 'Contiguous',
-          b : 'None-contiguous',
-          c : 'Both A and B',
-          d : 'None of the above'
-      },
+      question      : " Which type of memory is used by an Array in C++ programming language?",
+      options       : ['Contiguous','None-contiguous','Both A and B','None of the above'],
+      
       correctAnswer : 'a'
     },
     {
-      question : " Which types of arrays are always considered as linear arrays?",
-      answers  :{
-          a : 'Single-dimensional',
-          b : 'Multi-dimensional',
-          c : 'Both A and B',
-          d : 'None of the above'
-      },
+      question      : " Which types of arrays are always considered as linear arrays?",
+      options       : ['Single-dimensional','Multi-dimensional', 'Both A and B','None of the above'],
+        
       correctAnswer : 'a'
     },
 
@@ -70,18 +49,19 @@ var myquestions =[
 function main(){
   let score = 0 ;
 
-  for(var iterator = 0 ; iterator<myquestions.length; iterator++)
+  for(var iterator = 0 ; iterator<questions.length; iterator++)
   {
 
     showQuestion(iterator);
     console.log(`\n`);
-    showAnswers(iterator);
+    showOptions(iterator);
      console.log(`\n`);
     var letters = /^[a-d]+$/;
-     const user_answer = readLineSync.question('\nEnter your option: ' + '\n');
-     if(user_answer.match(letters))
+     const userAnswer = readLineSync.question('\nEnter your option: ' + '\n');
+    
+     if(userAnswer.match(letters))
      {
-        if(user_answer == myquestions[iterator].correctAnswer){
+        if(userAnswer === questions[iterator].correctAnswer){
          console.log(`Yay !! Correct Answer`+ '\n');
          score = score +1;
 
@@ -103,21 +83,22 @@ function main(){
 
 function showQuestion(questionnumber){
  
-    console.log(`${myquestions[questionnumber].question}`);
+    console.log(`${questions[questionnumber].question}`);
   
 }
 
-//showQuestion(myquestions);
-function showAnswers(questionnumber){
+function showOptions(questionnumber){
  
- for( var abc in myquestions[questionnumber].answers)
- {
- console.log(`${abc}  ${myquestions[questionnumber].answers[abc]}`);
- }
+for(var options = 97 ; options <= 100 ; options++){
+  	var showoptions = String.fromCharCode(options);
+    console.log(`${showoptions}. ${questions[questionnumber].options[options-97]}`);
+
+}
 }
 
 function DisplayScore(score){
-  console.log(` you scored ${score} out of ${myquestions.length}`);
+  console.log(` you scored ${score} out of ${questions.length}`);
 }
+
 welcome();
 
